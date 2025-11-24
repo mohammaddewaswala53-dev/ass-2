86df91f72dbabf779572cb076afe609949008e3e
mohammaddewaswala53-dev <mohammaddewaswala53@gmail.com>
2025-11-24 22:17:45 +0530

    Add files via upload

---
+++ even or odd program.py
@@ -0,0 +1,5 @@
+numbers=input("enter any number")
+if int(numbers)%2==0:
+    print(" the number is even")
+else:
+        print(" the number is odd")
---
+++ sum of 50 numbers.py
@@ -0,0 +1,4 @@
+num=int(1)
+for i in range(1):
+     total=(sum(range(1,51)))
+     print (f"The of 50 number is {total}")
